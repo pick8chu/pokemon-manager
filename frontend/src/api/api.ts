83b98ae2e0cf73a16adf72d6b70f2837a40c11b7
pick8chu <po00076@gmail.com>
2023-04-10 22:47:36 +0200

    changes

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 export const API: AxiosInstance = axios.create({
-  baseURL: 'http://api:8080',
+  baseURL: 'http://localhost:8080',
   headers: {
     'Content-Type': 'application/json',
   },
